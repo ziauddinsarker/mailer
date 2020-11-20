f6286f750d9491bc21b01fdf4ff3b8894b500991
Ziauddin Md <m-ziauddin@t-mark.co.jp>
2020-11-20 13:01:17 +0900

    Updates

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -22,15 +22,11 @@ class SendMailController extends Controller
             die("Could not connect to the database.  Please check your configuration. error:" . $e);
         }
 
-        //$users = DB::table('user')->where('user_id','=',37605)->get();
+        $users = DB::table('users')->get();
 
         $subject = env("SUBJECT", "【PPS■ニュース】　2020年11月10日　ナショジオ写真家 アイラ・ブロックの世界");
         $url = env("URL", "http://www.ppsjp.com/news/201110/distro.html");
         $group = env("GROUP", "0");
-        //\Mail::to('your_receiver_email@gmail.com')->send(new \App\Mail\MyTestMail($details));
-        print($subject);
-        print($url);
-        print($group);
 
         $recipients = [
             '0' => ['m-ziauddin@t-mark.co.jp','ziauddin.sarker@gmail.com'],
@@ -38,17 +34,11 @@ class SendMailController extends Controller
         ];
 
         $doc = phpQuery::newDocumentFileHTML($url);
-        $email = 'm-ziauddin@t-mark.co.jp';
-        if(in_array($email, $recipients[$group])) {
-//            dd(in_array($email, $recipients[$group]));
-        }
 
-
-        Mail::to($email)->send(new EmailDemo());
-
+       foreach ($users as $user) {
+           Mail::to($user->email)->send(new EmailDemo());
+       }
         return response()
             ->json(['message' => 'Email has been sent.']);
     }
-
-
 }
--- app/Mail/EmailDemo.php
+++ app/Mail/EmailDemo.php
@@ -21,6 +21,32 @@ class EmailDemo extends Mailable
     {
         //
     }
+    function rel2abs($rel, $base)
+    {
+        if(strpos($rel,"//")===0)
+        {
+            return "http:".$rel;
+        }
+
+        /* return if  already absolute URL */
+        if  (parse_url($rel, PHP_URL_SCHEME) != '') return $rel;
+        /* queries and  anchors */
+        if ($rel[0]=='#'  || $rel[0]=='?') return $base.$rel;
+        /* parse base URL  and convert to local variables:
+        $scheme, $host,  $path */
+        extract(parse_url($base));
+        /* remove  non-directory element from path */
+        $path = preg_replace('#/[^/]*$#',  '', $path);
+        /* destroy path if  relative url points to root */
+        if ($rel[0] ==  '/') $path = '';
+        /* dirty absolute  URL */
+        $abs =  "$host$path/$rel";
+        /* replace '//' or  '/./' or '/foo/../' with '/' */
+        $re =  array('#(/.?/)#', '#/(?!..)[^/]+/../#');
+        for($n=1; $n>0;  $abs=preg_replace($re, '/', $abs, -1, $n)) {}
+        /* absolute URL is  ready! */
+        return  $scheme.'://'.$abs;
+    }
 
     /**
      * Build the message.
@@ -30,38 +56,11 @@ class EmailDemo extends Mailable
     public function build()
     {
         $subject = '【PPS■ニュース】　2020年11月10日　ナショジオ写真家 アイラ・ブロックの世界';
-        $url = 'http://ziauddins.com/';
+        $url = 'http://tmark-lp.stage3.t-mark.co.jp/';
         $doc = phpQuery::newDocumentFileHTML($url,'utf-8');
-        function rel2abs($rel, $base)
-        {
-            if(strpos($rel,"//")===0)
-            {
-                return "http:".$rel;
-            }
-
-            /* return if  already absolute URL */
-            if  (parse_url($rel, PHP_URL_SCHEME) != '') return $rel;
-            /* queries and  anchors */
-            if ($rel[0]=='#'  || $rel[0]=='?') return $base.$rel;
-            /* parse base URL  and convert to local variables:
-            $scheme, $host,  $path */
-            extract(parse_url($base));
-            /* remove  non-directory element from path */
-            $path = preg_replace('#/[^/]*$#',  '', $path);
-            /* destroy path if  relative url points to root */
-            if ($rel[0] ==  '/') $path = '';
-            /* dirty absolute  URL */
-            $abs =  "$host$path/$rel";
-            /* replace '//' or  '/./' or '/foo/../' with '/' */
-            $re =  array('#(/.?/)#', '#/(?!..)[^/]+/../#');
-            for($n=1; $n>0;  $abs=preg_replace($re, '/', $abs, -1, $n)) {}
-            /* absolute URL is  ready! */
-            return  $scheme.'://'.$abs;
-        }
-
         foreach($doc['img'] as $img) {
             $src = $img->getAttribute('src');
-            $src = rel2abs($src, $url);
+            $src = $this->rel2abs($src, $url);
             $img->setAttribute('src', $src);
         }
         return $this->subject($subject)->html($doc);
